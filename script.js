38d44c2d420c541d5609a5cc94de54d023ee711e
itsonlythebeginning <107440223+itsonlythebeginning@users.noreply.github.com>
2022-10-03 18:40:14 +0300

    init

---
+++ script.js
@@ -0,0 +1,64 @@
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+let header_burger = document.querySelector(".header_burger")
+
+let header_nav_two = document.querySelector(".header_nav_two")
+let header_nav_close = document.querySelector(".header_nav_close")
+
+header_burger.addEventListener("click", function () {
+    console.log(2)
+    header_nav_two.classList.add("header_list_active")
+
+})
+
+header_nav_close.addEventListener("click", function () {
+    console.log(1)
+    header_nav_two.classList.remove("header_list_active")
+})
